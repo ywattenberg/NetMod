48e69197a720e547fc5fa5e463d35ff11995dd5f
fkahlbacher <fabian.kahlbacher@bluewin.ch>
2021-10-28 18:56:15 +0200

    first draft task 2

--- Assig_02/MHSim.R
+++ Assig_02/MHSim.R
@@ -13,6 +13,21 @@
 
 # Output: next state of the chain
 
+z1 <- function(net){
+  return(sum(net))
+}
+
+z2 <- function(net){
+  sum <- 0
+  nvertices <- nrow(net)
+  for (i in 1:nvertices) {
+    for (j in 1:i) {
+      sum <- sum + net[i,j] * net[j,i] 
+    }
+  }
+  return(sum)
+}
+
 MHstep <- function(net, theta1, theta2){
   
   # Number of vertices in the network
@@ -26,17 +41,28 @@ MHstep <- function(net, theta1, theta2){
   # Compute the change statistics
   
   #                --- MISSING---
+  stat <- exp(theta1 * z1(net) + theta2 * z2(net))
+  
+  net[tie[1],tie[2]] <- !net[tie[1],tie[2]] # flip a tie
+  change_stat <- exp(theta1 * z1(net) + theta2 * z2(net))
+  # ***************************************************************
+  ## ask what they exactly want for the change statistics
+  # ***************************************************************
   
   
   # Compute the probability of the next state 
   # according to the Metropolis-Hastings algorithm
   
   #                --- MISSING---
+  acceptance_ratio <- change_stat/(stat + change_stat)
   
   # Select the next state: 
 
   #                --- MISSING---
   
+  if (runif(1) > acceptance_ratio) {
+    net[tie[1], tie[2]] <- !net[tie[1], tie[2]] # flip it back
+  }
   # Return the next state of the chain
   return (net)
 }
@@ -67,7 +93,10 @@ MarkovChain <- function(net, theta1, theta2, burnin=10000,
   
   # Perform the burnin steps
   #                --- MISSING---
-  
+  for (burninStep in 1:burnin) {
+    net <- MHstep(net, theta1, theta2)
+  }
+
   # After the burnin phase we draw the networks
   # The simulated networks and statistics are stored in the objects
   # netSim and statSim
@@ -77,7 +106,62 @@ MarkovChain <- function(net, theta1, theta2, burnin=10000,
   netCounter <- 1 # counter for the number of simulated network
   
   #                --- MISSING---
+  for (netCounter in 1:nNet) {
+    for(thinningSteps in 1:thinning) {
+      net <- MHstep(net, theta1, theta2)
+    }
+    netSim[,,netCounter] <- net
+    statSim[netCounter,] <- c(z1(net), z2(net))
+  }
+  # ***************************************************************
+  ## ask: probabilities or counts??
+  # ***************************************************************
   
   # Return the simulated networks and the statistics
   list(netSim=netSim, statSim=statSim)
 }
+
+#setwd("C:/Projects/NetMod/Assig_02/")
+communication <- as.matrix(read.csv("data/communication.csv", header=F))
+
+
+net_0 <- matrix(0, 38,38)
+
+sim_net <- MarkovChain(
+  net=net_0,
+  theta1=-0.6,
+  theta2=1.5,
+)
+
+sim_net_2 <- MarkovChain(
+  net=net_0,
+  theta1=-1.737,
+  theta2=1.632,
+)
+
+mean(sim_net_2$statSim[,1])
+mean(sim_net_2$statSim[,2])
+
+mean(sim_net$statSim[,1])
+mean(sim_net$statSim[,2])
+
+z1(communication)
+z2(communication)
+
+
+
+dim(sim_net$netSim)
+
+sim_net$netSim[,,1]
+
+plot(sim_net$statSim[,1], sim_net$statSim[,2], type='l')
+
+
+# compare with ergm
+library(network) 
+library(ergm)
+netw <- network(communication, directed=TRUE)
+
+ergm(netw ~ edges + mutual) #gwesp(decay=0.3,fixed=TRUE)
+#edges    mutual  
+#-1.737   1.632
